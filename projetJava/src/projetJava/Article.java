5b16cd205230161f099abe5da5a8372d7b601c46
E146307E <E146307E@U-INF-J-E114-09.iut-nantes.univ-nantes.prive>
2016-10-03 16:13:01 +0200

    encore un test

--- projetJava/src/projetJava/Article.java
+++ projetJava/src/projetJava/Article.java
@@ -1,5 +1,56 @@
 package projetJava;
 
 public class Article {
+	
+	protected String reference;
+	protected String marque;
+	protected String intitule;
+	protected double prixParJour;
+	protected int nbEnStock;
+	
+	// constructeur
+	public Article(String reference, String marque, String intitule, double prixParJour, int nbEnStock) {
+		this.reference = reference;
+		this.marque = marque;
+		this.intitule = intitule;
+		this.prixParJour = prixParJour;
+		this.nbEnStock = nbEnStock;
+	}
 
+	// getters et setters
+	public String getReference() {
+		return reference;
+	}
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	public String getMarque() {
+		return marque;
+	}
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+	public String getIntitule() {
+		return intitule;
+	}
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+	public double getPrixParJour() {
+		return prixParJour;
+	}
+	public void setPrixParJour(double prixParJour) {
+		this.prixParJour = prixParJour;
+	}
+	public int getNbEnStock() {
+		return nbEnStock;
+	}
+	public void setNbEnStock(int nbEnStock) {
+		this.nbEnStock = nbEnStock;
+	}
+	
+	
+	
+	
+// fin classe	
 }
---
+++ projetJava/src/projetJava/Magasin.java
@@ -0,0 +1,7 @@
+package projetJava;
+import java.util.List;
+
+public class Magasin {
+	
+	protected List<Article> listeArticles;
+}
