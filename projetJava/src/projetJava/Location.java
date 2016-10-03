7dae6ff15db0b2dd54b832c64564b5b052e67343
E122692L <E122692L@U-INF-J-E114-08.iut-nantes.univ-nantes.prive>
2016-10-03 17:33:34 +0200

    méthode calculMontant de location

--- projetJava/src/projetJava/Location.java
+++ projetJava/src/projetJava/Location.java
@@ -9,9 +9,9 @@ public class Location {
 	private Date dateDebut;
 	private Date dateFin;
 	private double montant;
-	private ArrayList <Article> lesArticles;
-	
-	//Constructeur
+	private ArrayList<Article> lesArticles;
+
+	// Constructeur
 	public Location(Date dateDebut, Date dateFin, double montant,
 			ArrayList<Article> lesArticles) {
 		this.dateDebut = dateDebut;
@@ -19,42 +19,56 @@ public class Location {
 		this.montant = montant;
 		this.lesArticles = lesArticles;
 	}
-	
-	//Getters et Setters
+
+	// Getters et Setters
 	public Date getDateDebut() {
 		return dateDebut;
 	}
+
 	public void setDateDebut(Date dateDebut) {
 		this.dateDebut = dateDebut;
 	}
+
 	public Date getDateFin() {
 		return dateFin;
 	}
+
 	public void setDateFin(Date dateFin) {
 		this.dateFin = dateFin;
 	}
+
 	public double getMontant() {
 		return montant;
 	}
+
 	public void setMontant(double montant) {
 		this.montant = montant;
 	}
+
 	public ArrayList<Article> getLesArticles() {
 		return lesArticles;
 	}
+
 	public void setLesArticles(ArrayList<Article> lesArticles) {
 		this.lesArticles = lesArticles;
 	}
-	
-	//Calcul du montant de la location en fonction du prix par jour des articles et de la durée
-	public void calculMontant(){
-		double prix=0;
-		long diff = this.dateDebut.getTime( ) - this.getDateFin().getTime( );
-		for(Article article :this.lesArticles ){
-			prix+=article.getPrixParJour()*diff;
+
+	// Calcul du montant de la location en fonction du prix par jour des
+	// articles et de la durée
+	public void calculMontant() {
+		double prix = 0;
+		long diff = this.dateDebut.getTime() - this.getDateFin().getTime()/86399993;
+		for (Article article : this.lesArticles) {
+			prix += article.getPrixParJour() * diff;
 		}
-		this.setMontant(diff);
+		this.setMontant(prix);
+		System.out.println(diff);
 	}
+
+
+	
+	
+	
 	
 
 }
