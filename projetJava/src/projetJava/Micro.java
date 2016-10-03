54614a65b9864becdbb27e844eb4546e2f81a0ac
E146307E <E146307E@U-INF-J-E114-09.iut-nantes.univ-nantes.prive>
2016-10-03 16:45:37 +0200

    classe en plus

--- projetJava/src/projetJava/Micro.java
+++ projetJava/src/projetJava/Micro.java
@@ -4,11 +4,20 @@ public class Micro extends Article{
 	
 	protected boolean sansFil;
 
+	// constructeur
 	public Micro(String reference, String marque, String intitule,
-			double prixParJour, int nbEnStock) {
+			double prixParJour, int nbEnStock, boolean sansFil) {
 		super(reference, marque, intitule, prixParJour, nbEnStock);
+		this.sansFil = sansFil;
+	}
 
+	// getter et setter
+	public boolean isSansFil() {
+		return sansFil;
+	}
+	public void setSansFil(boolean sansFil) {
+		this.sansFil = sansFil;
 	}
 	
-	
+// fin classe	
 }
